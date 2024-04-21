6eba9545a14db1761e2946c005d6010c273595ef
Ta1kie <vargaradovan@pm.me>
2024-04-21 19:00:15 +0200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,27 @@
+# This is my roadtrip though the Python language that I want to learn for my career transformation from Manual QA to IT Support and then Cloud engineering
+
 my_name = "Radovan Varga"
 print ("Hello and welcome my name is " + my_name + " and this is my first code written in Python!")
+print("Hello world!")
+
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "Roast beef with mashed potatoes"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal = "Beeeeeaans"
+# Printing out dinner
+print("Dinner:")
+print(meal)
+
+
