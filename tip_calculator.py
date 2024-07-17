7f72a1e9b3bad90b3e42bba9817ae5a8f8ee650f
Ta1kie <vargaradovan@pm.me>
2024-07-17 23:12:22 +0200

    .

--- tip_calculator.py
+++ tip_calculator.py
@@ -6,7 +6,6 @@ print(type(bill))
 tip = int(input("How much tip would you like to give? 25, 50, 100 or 200?"))
 people = int(input("How many people to split the bill?"))
 bill_with_tip = tip + bill
-#print(bill_with_tip)
 bill_per_person = bill_with_tip / people
 final_amount = round(bill_per_person, 2)
 final_amount = "{:.2f}".format(bill_per_person)
